2322b9899f6f14afae0aca6defe3114a4e9dfdbf
password442619 <42017683+password442619@users.noreply.github.com>
2018-08-16 15:31:55 +0800

    Delete download_code.sh

--- download_code.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/expect
-set username "motb"
-set passwd "motb1234"
-spawn git clone http://gitlab.asoco.com.cn/asoco/asoco-wx-app.git
-expect "Username:"
-send "$username\n"
-expect "Password"
-send "$passwd\n"
-interact

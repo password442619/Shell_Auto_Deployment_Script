a8e18348065a92d2848dea6a5b2d1d8425e47294
password442619 <42017683+password442619@users.noreply.github.com>
2018-10-26 15:42:41 +0800

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -1,5 +1,7 @@
-yum install -y yum-utils device-mapper-persistent-data lvm2
-yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-yum makecache fast
-yum -y install docker-ce
-systemctl start docker
+
+docker run hello-world
+if [ $? -eq 0 ]; then
+	echo 'docker has been installed.'
+else
+	yum install -y yum-utils device-mapper-persistent-data lvm2 && yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo && yum makecache fast && yum -y install docker-ce && systemctl start docker
+fi

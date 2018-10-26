f74cab21dbae3d687e98e5a0c96f3c7ad598998c
password442619 <42017683+password442619@users.noreply.github.com>
2018-10-26 15:35:24 +0800

    Create docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,5 @@
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+yum makecache fast
+yum -y install docker-ce
+systemctl start docker

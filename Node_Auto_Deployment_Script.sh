8a04b60c4a7306bc24019459fbf3ac8b6fc031ef
password442619 <42017683+password442619@users.noreply.github.com>
2018-10-31 16:30:46 +0800

    Update Node_Auto_Deployment_Script.sh

--- Node_Auto_Deployment_Script.sh
+++ Node_Auto_Deployment_Script.sh
@@ -11,7 +11,7 @@ mv node-v8.11.2-linux-x64 node && cp -r node /usr/local/
 p=`grep NODE_HOME /etc/profile|wc -l`
 if [ $p -eq 0 ]; then
   echo "export NODE_HOME=/usr/local/node" >> /etc/profile
-  ehco "export PATH=\$NODE_HOME/bin:\$PATH" >> /etc/profile
+  echo "export PATH=\$NODE_HOME/bin:\$PATH" >> /etc/profile
 else
   echo "node home has been setted!"
 fi

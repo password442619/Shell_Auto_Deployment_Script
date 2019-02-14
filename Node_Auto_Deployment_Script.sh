7ace631f2631fa17e86f8cf3428b20aa64f5b22d
password442619 <42017683+password442619@users.noreply.github.com>
2019-02-14 10:00:19 +0800

    Update Node_Auto_Deployment_Script.sh

--- Node_Auto_Deployment_Script.sh
+++ Node_Auto_Deployment_Script.sh
@@ -12,9 +12,9 @@ p=`grep NODE_HOME /etc/profile|wc -l`
 if [ $p -eq 0 ]; then
   echo "export NODE_HOME=/usr/local/node" >> /etc/profile
   echo "export PATH=\$NODE_HOME/bin:\$PATH" >> /etc/profile
-  source /etc/profile
 else
   echo "node home has been setted!"
 fi
 
 rm -fr node*
+source /etc/profile

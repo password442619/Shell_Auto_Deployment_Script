d8abb37ef9f06861663f5a1923e693e4577dcdce
password442619 <42017683+password442619@users.noreply.github.com>
2019-02-15 14:43:54 +0800

    Update Node_Auto_Deployment_Script.sh

--- Node_Auto_Deployment_Script.sh
+++ Node_Auto_Deployment_Script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-if [ -e node-v8.11.2-linux-x64.tar.gz ]; then
-  echo "node-v8.11.2-linux-x64.tar.gz file has existed."
+if [ -e node-v10.11.0-linux-x64.tar.gz ]; then
+  echo "node-v10.11.0-linux-x64.tar.gz file has existed."
 else
-  wget https://nodejs.org/download/release/v8.11.2/node-v8.11.2-linux-x64.tar.gz
+  wget https://nodejs.org/download/release/v10.11.0/node-v10.11.0-linux-x64.tar.gz
 fi
 
-tar zxvf node-v8.11.2-linux-x64.tar.gz > /dev/null
-mv node-v8.11.2-linux-x64 node && cp -r node /usr/local/
+tar zxvf node-v10.11.0-linux-x64.tar.gz > /dev/null
+mv node-v10.11.0-linux-x64 node && cp -r node /usr/local/
 
 p=`grep NODE_HOME /etc/profile|wc -l`
 if [ $p -eq 0 ]; then

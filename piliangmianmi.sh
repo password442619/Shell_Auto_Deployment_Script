eb07bfddb11bac3ee20fe46e01d8edc1956a0f10
password442619 <42017683+password442619@users.noreply.github.com>
2019-03-26 13:40:55 +0800

    Update piliangmianmi.sh

--- piliangmianmi.sh
+++ piliangmianmi.sh
@@ -2,25 +2,22 @@
 
 创建fenfa.sh
 
-#!/bin/bash  
+#!/bin/bash
 for p in $(cat /opt/work/ip.txt)
 do
-ip=$(echo "$p"|awk -F '[:]' '{print $1}')
-password=$(echo "$p"|awk -F '[:]' '{print $2}')
-expect -c "
- spawn ssh-copy-id -i /root/.ssh/id_rsa.pub root@$ip  
-          expect {   
-                  \"*yes/no*\" {send \"yes\r\"; exp_continue}   
-                  \"*password*\" {send \"$password\r\"; exp_continue}   
-                  \"*Password*\" {send \"$password\r\";}   
-          }   
-  "
-done    
-
-
+    ip=$(echo "$p"|awk -F '[:]' '{print $1}')
+    password=$(echo "$p"|awk -F '[:]' '{print $2}')
+    expect -c "
+    spawn ssh-copy-id -i /root/.ssh/id_rsa.pub root@$ip
+          expect {
+               \"*yes/no*\" {send \"yes\r\"; exp_continue}
+               \"*password*\" {send \"$password\r\"; exp_continue}
+               \"*Password*\" {send \"$password\r\";}
+          }
+    "
+done
 
 创建ip.txt
-
 192.168.10.100:abc.123456
 192.168.60.123:password.
 192.168.60.124:Abc.123456

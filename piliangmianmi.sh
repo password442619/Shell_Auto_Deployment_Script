30dd46dfa62cbd47869b4f0715473442dfccac70
password442619 <42017683+password442619@users.noreply.github.com>
2019-03-26 13:32:59 +0800

    Update piliangmianmi.sh

--- piliangmianmi.sh
+++ piliangmianmi.sh
@@ -2,12 +2,11 @@
 
 创建fenfa.sh
 
-```
 #!/bin/bash  
 for p in $(cat /opt/work/ip.txt)
 do
-ip=$(echo "$p"|cut -f1)
-password="Oct11.abc"
+ip=$(echo "$p"|awk -F '[:]' '{print $1}')
+password=$(echo "$p"|awk -F '[:]' '{print $2}')
 expect -c "
  spawn ssh-copy-id -i /root/.ssh/id_rsa.pub root@$ip  
           expect {   
@@ -15,14 +14,14 @@ expect -c "
                   \"*password*\" {send \"$password\r\"; exp_continue}   
                   \"*Password*\" {send \"$password\r\";}   
           }   
-  "   
-  done    
-```
+  "
+done    
+
+
 
 创建ip.txt
 
-```
-192.168.10.100
-192.168.60.123
-192.168.60.124
-192.168.10.101
+192.168.10.100:abc.123456
+192.168.60.123:password.
+192.168.60.124:Abc.123456
+192.168.10.101:Samsung.123

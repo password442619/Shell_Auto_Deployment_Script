daf9210c2b9c8286f57d1f351a75f170d80664c9
password442619 <42017683+password442619@users.noreply.github.com>
2019-03-26 13:45:45 +0800

    Update SSH_free_density.sh

--- SSH_free_density.sh
+++ SSH_free_density.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ -e ~/.ssh/ ]; then
   echo ".ssh has exsit."
 else
@@ -6,3 +7,4 @@ fi
 cd ~/.ssh/
 ssh-keygen -t rsa
 ssh-copy-id -i ~/.ssh/id_rsa.pub $1@$2
+#脚本后跟账号和密码，例如./SSH_free_density.sh root 123456

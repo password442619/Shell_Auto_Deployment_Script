d147972348f969871f2b188acf84148ba8b09e9f
password442619 <42017683+password442619@users.noreply.github.com>
2019-05-10 10:29:01 +0800

    Update and rename ali_yum_centos7-3.sh to ali_yum_centos6and7.sh

--- ali_yum_centos6and7.sh
+++ ali_yum_centos6and7.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget -O /etc/yum.repos.d/Centos-7.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+wget -O /etc/yum.repos.d/Centos-6.repo http://mirrors.aliyun.com/repo/Centos-6.repo
 yum clean all
 yum makecache fast
 yum install -y epel-release

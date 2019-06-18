52214b810eaaf13ea1df34ef9dd220a2ebb0bef4
password442619 <42017683+password442619@users.noreply.github.com>
2019-06-18 14:50:27 +0800

    Create scan_port.py

---
+++ scan_port.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+import sys
+import nmap
+
+scan_row = []
+input_data = raw_input('Please input hosts and port:')
+scan_row = input_data.split(" ")
+if len(scan_row)!=2:
+	print "Input errors,example \"192.168.2.0/24 80,443,22\""
+	sys.exit(0)
+hosts = scan_row[0]
+port = scan_row[1]

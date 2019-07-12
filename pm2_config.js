212f56fec672372658f189a80205dbdd0db150a9
password442619 <42017683+password442619@users.noreply.github.com>
2019-07-12 17:51:08 +0800

    Update pm2_config.js

--- pm2_config.js
+++ pm2_config.js
@@ -57,3 +57,23 @@ module.exports = {
 
 详细的配置文件写法，请参考：
 https://pm2.io/doc/zh/runtime/guide/ecosystem-file/
+
+参数解释：
+apps:json结构，apps是一个数组，每一个数组成员就是对应一个pm2中运行的应用
+name:应用程序名称
+cwd:应用程序所在的目录
+script:应用程序的脚本路径
+log_date_format:
+error_file:自定义应用程序的错误日志文件
+out_file:自定义应用程序日志文件
+pid_file:自定义应用程序的pid文件
+instances:启动的线程数量
+min_uptime:最小运行时间，这里设置的是60s即如果应用程序在60s内退出，pm2会认为程序异常退出，此时触发重启max_restarts设置数量
+max_restarts:设置应用程序异常退出重启的次数，默认15次（从0开始计数）
+cron_restart:定时启动，解决重启能解决的问题
+watch:是否启用监控模式，默认是false。如果设置成true，当应用程序变动时，pm2会自动重载。这里也可以设置你要监控的文件。
+merge_logs:
+exec_interpreter:应用程序的脚本类型，默认是nodejs
+exec_mode:应用程序启动模式，默认是fork
+autorestart:启用/禁用应用程序崩溃或退出时自动重启
+vizion:启用/禁用vizion特性(版本控制)

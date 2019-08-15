483c0ce18de4a3d5d1d0dd9817edbde1559c79e4
password442619 <42017683+password442619@users.noreply.github.com>
2019-08-15 14:35:48 +0800

    Update piliangmianmi.sh

--- piliangmianmi.sh
+++ piliangmianmi.sh
@@ -1,24 +1,24 @@
-在 /opt/work目录下
+在 /mnt目录下，前提要有expect命令
 
 创建fenfa.sh
 
 #!/bin/bash
-for p in $(cat /opt/work/ip.txt)
+for a in $(cat /mnt/ip.txt)
 do
-    ip=$(echo "$p"|awk -F '[:]' '{print $1}')
-    password=$(echo "$p"|awk -F '[:]' '{print $2}')
-    expect -c "
-        spawn ssh-copy-id -i /root/.ssh/id_rsa.pub root@$ip
-        expect {
-            \"*yes/no*\" {send \"yes\r\"; exp_continue}
-            \"*password*\" {send \"$password\r\"; exp_continue}
-            \"*Password*\" {send \"$password\r\";}
-        }
-    "
+	ip=$(echo $a|awk -F '[:]' '{print $1}')
+	password=$(echo $a|awk -F '[:]' '{print $2}')
+	
+	expect -c "
+	spawn ssh-copy-id -i /root/.ssh/id_rsa.pub root@$ip
+	expect {
+		\"*yes/no*\" {send \"yes\r\"; exp_continue;}
+		\"*password*\" {send \"$password\r\";}
+		}
+	expect eof
+	"
 done
 
 创建ip.txt
 192.168.10.100:abc.123456
 192.168.60.123:password.
-192.168.60.124:Abc.123456
-192.168.10.101:Samsung.123
+IP:密码

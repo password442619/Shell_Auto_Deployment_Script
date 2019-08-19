067172d1c83921ba63645262e25b15d23358d9eb
password442619 <42017683+password442619@users.noreply.github.com>
2019-08-19 11:56:26 +0800

    Update Nginx_Auto_Deployment_Script.sh

--- Nginx_Auto_Deployment_Script.sh
+++ Nginx_Auto_Deployment_Script.sh
@@ -1,8 +1,8 @@
   #/bin/bash
   yum makecache fast > /dev/null
   yum install gcc pcre-devel zlib-devel -y > /dev/null
-  wget http://nginx.org/download/nginx-1.10.2.tar.gz > /dev/null
-  tar zxvf nginx-1.10.2.tar.gz > file.txt
-  cd nginx-1.10.2
+  wget http://nginx.org/download/nginx-1.14.2.tar.gz > /dev/null
+  tar zxvf nginx-1.14.2.tar.gz > file.txt
+  cd nginx-1.14.2
   ./configure && make && make install > /dev/null
   /usr/local/nginx/sbin/nginx

2fb63d45bb0c8957837c1cd7aca1146b46756769
password442619 <42017683+password442619@users.noreply.github.com>
2019-09-09 11:21:04 +0800

    结构化需要安装的依赖

---
+++ boyun_jiegouhua_script.sh
@@ -0,0 +1,1 @@
+yum  install autoconf automake gcc gcc-c++ gdb net-tools vim lrzsz zlib-devel bzip2-devel zip unzip libuuid-devel python-devel opencv-devel freetype-devel apr-devel curl-devel tbb-devel  glib2-devel gstreamer1-devel gstreamer1-plugins-base-devel kernel-devel kernel-headers xvidcore-devel python-paramiko python2-xlrd python-xlwt python2-numpy sysstat -y

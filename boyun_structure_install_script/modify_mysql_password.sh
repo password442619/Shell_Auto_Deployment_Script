27dcbe6e85664fe94820a0198f6538657eb7c119
password442619 <42017683+password442619@users.noreply.github.com>
2020-01-02 09:18:23 +0800

    初始化数据库脚本

---
+++ boyun_structure_install_script/modify_mysql_password.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+username=root
+password=$1
+expect << EOF
+	set timeout 1
+	spawn mysql -u $username -p 
+	expect {
+		"Enter password:" {send "$password\r"; exp_continue;}
+	}
+	expect "mysql>" {send "set password=password(\"Boyun@2019\");\r";}
+	expect "mysql>" {send "create database if not exists vss;\r";}
+	expect "mysql>" {send "flush privileges;\r";}
+	expect "mysql>" {send "exit;\r";}
+EOF
+exit

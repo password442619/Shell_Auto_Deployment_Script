92320f034d93a28c0fe2edef11ef0f3dba415cb1
password442619 <42017683+password442619@users.noreply.github.com>
2020-09-01 17:13:50 +0800

    更新免密登录脚本使用说明

--- SSH_free_density.sh
+++ SSH_free_density.sh
@@ -7,5 +7,5 @@ fi
 cd ~/.ssh/
 ssh-keygen -t rsa
 ssh-copy-id -i ~/.ssh/id_rsa.pub $1@$2
-#脚本后跟账号和密码
-#例如./SSH_free_density.sh root 123456
+#脚本后跟账号和IP地址
+#例如./SSH_free_density.sh root 192.168.X.X(IP地址)
